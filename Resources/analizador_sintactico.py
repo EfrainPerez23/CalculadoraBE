c98263574485c9946fc11322b7641cc24325b8b5
Efrain Perez <efrain.abperez23@gmail.com>
2018-11-07 01:21:35 -0500

    fixed problem with equation

--- Resources/analizador_sintactico.py
+++ Resources/analizador_sintactico.py
@@ -6,10 +6,8 @@ from flask_restful import Resource, reqparse
 
 # resultado del analisis
 resultado_gramatica = []
-
+gramatic_result = {'errors': [], 'value': ''}
 precedence = (
-    ('right', 'IDENTIFICADOR'),
-    ('right', 'ASIGNAR'),
     ('left', 'SUMA', 'RESTA'),
     ('left', 'MULT', 'DIV'),
     ('left', 'POTENCIA', 'MODULO'),
@@ -19,9 +17,9 @@ precedence = (
 )
 nombres = {}
 
-def p_declaracion_igual(t):
-   '''declaracion : IDENTIFICADOR ASIGNAR expresion'''
-   nombres[t[1]] = t[3]
+#def p_declaracion_igual(t):
+#    'declaracion : IDENTIFICADOR expresion IGUAL'
+#    nombres[t[1]] = t[3]
 
 def p_declaracion_expr(t):
     'declaracion : expresion'
@@ -30,7 +28,7 @@ def p_declaracion_expr(t):
 
 def p_expresion_operaciones(t):
     '''
-    expresion   :   expresion SUMA expresion
+    expresion  :   expresion SUMA expresion
                 |   expresion RESTA expresion
                 |   expresion MULT expresion
                 |   expresion DIV expresion
@@ -38,19 +36,17 @@ def p_expresion_operaciones(t):
                 |   expresion MODULO expresion
 
     '''
-    
     if t[2] == '+':
-        print(t)
         t[0] = t[1] + t[3]
-    elif t[4] == '-':
+    elif t[2] == '-':
         t[0] = t[1] - t[3]
-    elif t[4] == '*':
+    elif t[2] == '*':
         t[0] = t[1] * t[3]
-    elif t[4] == '/':
+    elif t[2] == '/':
         t[0] = t[1] / t[3]
-    elif t[4] == '%':
+    elif t[2] == '%':
         t[0] = t[1] % t[3]
-    elif t[4] == '**' or t[4] == '^':
+    elif t[2] == '**' or t[2] == '^':
         i = t[3]
         t[0] = t[1]
         while i > 1:
@@ -62,11 +58,11 @@ def p_expresion_trig(t):
                 | cos PARIZQ expresion PARDER
                 | tan PARIZQ expresion PARDER'''
     if t[1] == "sin":
-        t[0] = math.sin(t[3])
+        t[0] = math.sin(math.radians(t[3]))
     elif t[1] == "cos":
-        t[0] = math.cos(t[3])
+        t[0] = math.cos(math.radians(t[3]))
     elif t[1] == "tan":
-        t[0] = math.tan(t[3])
+        t[0] = math.tan(math.radians(t[3]))
 
 def p_expresion_grupo(t):
     'expresion  : PARIZQ expresion PARDER'
@@ -87,22 +83,31 @@ def p_expresion_numero(t):
 
 def p_error(t):
     global resultado_gramatica
+    global gramatic_result
     if t:
         resultado = "Error sintactico de tipo {} en el valor {}".format( str(t.type),str(t.value))
-        print(resultado)
+        gramatic_result['errors'].append({
+        'type': str(t.type),
+        'value': str(t.value)
+        })
     else:
         resultado = "Error sintactico {}".format(t)
+        gramatic_result['errors'].append({
+        'type': str(t.type),
+        'value': str(t.value)
+        })
         print(resultado)
+    
     resultado_gramatica.append(resultado)
-
-
 
 # instanciamos el analizador sintactico
 parser = yacc.yacc()
 
 def prueba_sintactica(data):
     global resultado_gramatica
-    data = data.strip()
+    global gramatic_result
+    gramatic_result['value'] = ''
+    gramatic_result['errors'].clear()
     resultado_gramatica.clear()
 
     for item in data.splitlines():
@@ -110,9 +115,11 @@ def prueba_sintactica(data):
             gram = parser.parse(item)
             if gram:
                 resultado_gramatica.append(str(gram))
+                gramatic_result['value'] = gram
         else: print("data vacia")
 
-    return resultado_gramatica
+    print("result: ", resultado_gramatica)
+    return gramatic_result
 
         
 
